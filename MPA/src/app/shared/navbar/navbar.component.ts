8e51e5141fde75eabe5aca9b172afd3cd25adee3
ZarateSofia <118627198+ZarateSofia@users.noreply.github.com>
2023-11-29 10:22:36 -0500

    Update navbar.component.ts

--- MPA/src/app/shared/navbar/navbar.component.ts
+++ MPA/src/app/shared/navbar/navbar.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLinkActive, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule,RouterLinkActive,RouterLink],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
