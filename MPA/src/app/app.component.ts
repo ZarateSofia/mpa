08a2a33d98e5098e82dc867a46d7a979693e5d67
Carlos <sdfsdk@gmail.com>
2023-11-29 10:25:30 -0500

    Componente Charts

--- MPA/src/app/app.component.ts
+++ MPA/src/app/app.component.ts
@@ -5,11 +5,11 @@ import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { IndexComponent } from './pages/index/index.component';
-
+import { ChartsComponent } from './pages/charts/charts.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, SidebarComponent,NavbarComponent,FooterComponent,IndexComponent],
+  imports: [CommonModule, RouterOutlet, SidebarComponent,NavbarComponent,FooterComponent,IndexComponent, ChartsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
