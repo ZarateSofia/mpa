45ea3e428b84b7e7374edf7a4ec04b53aa1f5b35
ZarateSofia <118627198+ZarateSofia@users.noreply.github.com>
2023-11-29 10:01:37 -0500

    index

--- MPA/src/app/app.component.ts
+++ MPA/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { IndexComponent } from './pages/index/index.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, SidebarComponent,NavbarComponent,FooterComponent],
+  imports: [CommonModule, RouterOutlet, SidebarComponent,NavbarComponent,FooterComponent,IndexComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
