7f58fa56638bfa7557c0bd41338c5e3082fa5c8f
Angello de Jesus Bravo Mera <36601894+sAngello31@users.noreply.github.com>
2023-11-29 10:11:08 -0500

    Ruteo

--- MPA/src/app/app.routes.ts
+++ MPA/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { IndexComponent } from './pages/index/index.component';
+import { ChartsComponent } from './pages/charts/charts.component';
+import { TablesComponent } from './pages/tables/tables.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: IndexComponent },
+    {path:  'about', component: AboutComponent },
+    {path:  'charts', component: ChartsComponent},
+    {path: "tables", component: TablesComponent}
+];
---
+++ MPA/src/app/pages/tables/tables.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-tables',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './tables.component.html',
+  styleUrl: './tables.component.css'
+})
+export class TablesComponent {
+
+}
