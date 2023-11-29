2af3963fce5dc3c520e103d3360a46acb85a31c9
Carlos <sdfsdk@gmail.com>
2023-11-29 09:48:23 -0500

    Componente footer creado

--- MPA/src/app/app.component.ts
+++ MPA/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, SidebarComponent,NavbarComponent],
+  imports: [CommonModule, RouterOutlet, SidebarComponent,NavbarComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
