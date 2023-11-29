0148a5c31438778d59a7c3137126f9361fba0088
Carlos <sdfsdk@gmail.com>
2023-11-29 10:33:11 -0500

    Borrado del header

--- MPA/src/app/pages/charts/charts.component.ts
+++ MPA/src/app/pages/charts/charts.component.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-charts',
-  standalone: true,
+  standalone: true, 
   imports: [CommonModule],
   templateUrl: './charts.component.html',
   styleUrl: './charts.component.css'
