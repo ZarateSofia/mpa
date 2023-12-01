fe4ed105ad9d531df1aa2a924aa63a4be1b491f9
Angello de Jesus Bravo Mera <bravoangello31@gmail.com>
2023-12-01 09:29:37 -0500

    Enruramiento arreglado

--- MPA/src/app/shared/sidebar/sidebar.component.ts
+++ MPA/src/app/shared/sidebar/sidebar.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLinkActive, RouterLink } from '@angular/router';
+
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLinkActive, RouterLink],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
